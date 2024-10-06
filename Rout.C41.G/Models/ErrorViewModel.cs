b6cebf65be49ed7157e4daa4bd5db34fa03bb8ae
elkrnk2022 <elkrnk2022@DESKTOP-LUM6P28>
2024-10-06 13:19:10 +0300

    Add project files.

---
+++ Rout.C41.G/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rout.C41.G.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
